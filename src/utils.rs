5e6edab8a1e9b34d9589f36ef3b6f8d5c115e224
oonamo <onam75hernandez@gmail.com>
2025-01-11 13:53:29 -0800

    test(utils): Test `utils::str_vec_non_empty`
    Testing this as it *will* affect formatting if not properly implemented.
    TODO: Should this also check for whitespace characters such as `\n`?

--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,21 @@ pub fn u8_to_str(u8: &Vec<u8>) -> String {
 pub fn u8_to_str_vec(u8: Vec<u8>) -> Vec<String> {
     u8_to_str(&u8).lines().map(|s| s.to_string()).collect()
 }
+
+#[cfg(test)]
+mod test {
+    use crate::utils::str_vec_non_empty;
+
+    #[test]
+    fn empty_vec_returns_false() {
+        let mut empty_vec: Vec<String> = vec![];
+        assert!(str_vec_non_empty(&empty_vec) == false);
+
+        empty_vec.push("".to_string());
+        assert!(str_vec_non_empty(&empty_vec) == false);
+
+
+        empty_vec.push("\t\n".to_string());
+        assert!(str_vec_non_empty(&empty_vec) == true);
+    }
+}
