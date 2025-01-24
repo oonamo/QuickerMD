c5d7552fca6ee9982849df3c4b3813b8143e659f
oonamo <onam75hernandez@gmail.com>
2025-01-23 22:05:56 -0800

    create quick example, mainly for testing

---
+++ examples/python/1_matplotlib.py
@@ -0,0 +1,11 @@
+# Use the run script to run these examples, along with the default
+# Configuration!
+x = [1, 2, 3, 4]
+y = [1, 3, 2, 4]
+
+plt.plot(x, y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("QuickerMD's Awesome Plot")
+
+plt.show()
